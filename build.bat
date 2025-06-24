f6a0fe14f438a0f829a47c822ba0acc4fafb4263
Avexiis <samelykimmel@gmail.com>
2025-06-23 22:39:51 -0400

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,8 +1,6 @@
 @echo off
-REM Create bin directory if it does not exist
 if not exist "bin" mkdir bin
 
-REM Compile Java file, output class files to bin
 javac -d bin XRPCSwingDemo.java
 
 if %ERRORLEVEL% NEQ 0 (
--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @echo off
-REM Run the Java class from bin directory
 cd /d "%~dp0"
 if not exist "bin" (
     echo You must build the project first.
